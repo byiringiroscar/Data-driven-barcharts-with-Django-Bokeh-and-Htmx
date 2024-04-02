c835c55ae66cd36e57b058edfcda97cecc218789
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-02 13:08:02 +0200

    Add chart bar

--- gdp/views.py
+++ gdp/views.py
@@ -11,7 +11,7 @@ from bokeh.plotting import figure
 # Create your views here.
 def index(request):
     # define which year we want the data from
-    max_year = GDP.objects.aggregate(max_yr=Max('year'))
+    max_year = GDP.objects.aggregate(max_yr=Max('year'))['max_yr']
     year = request.GET.get('year', max_year)
 
     # define number of countries to fetch
@@ -23,11 +23,11 @@ def index(request):
     country_gdps = [d.gdp for d in gdps]
 
 
-    cds = ColumnDataSource(data=dict(country=country_names, gdp=country_gdps))
+    cds = ColumnDataSource(data=dict(country_names=country_names, country_gdps=country_gdps))
 
     fig = figure(x_range=country_names, plot_height=500, title=f'Top {count} GDPs ({year})')
 
-    fig.vbar(source=cds, x='country_names', y='country_gdps', width=0.8)
+    fig.vbar(source=cds, x='country_names', top='country_gdps', width=0.8)
 
     script, div = components(fig)
 
