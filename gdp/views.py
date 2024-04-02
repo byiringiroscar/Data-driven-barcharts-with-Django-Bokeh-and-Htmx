35b268db535609157f9f3b3553372b3c85fb07f5
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-02 12:36:20 +0200

    Update views.py

--- gdp/views.py
+++ gdp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Max
+from gdp.models import GDP
 
 # Create your views here.
 def index(request):
+    max_year = GDP.objects.aggregate(max_yr=Max('year'))
     return render(request, 'index.html')
