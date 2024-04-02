5e991e3a5df0e4138a86a93327ffe90b0d37f1c5
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-02 12:25:29 +0200

    Populate sample data

--- gdp/management/commands/populate.py
+++ gdp/management/commands/populate.py
@@ -1,8 +1,30 @@
 from django.core.management.base import BaseCommand
+from gdp.models import GDP
+from django.conf import settings
+import json
+import itertools
 
 
 class Command(BaseCommand):
     help = 'Load Courses and Modules'
 
     def handle(self, *args, **kwargs):
-        raise NotImplementedError
+        if not GDP.objects.count():
+            datafile = settings.BASE_DIR / 'data' / 'gdp.json'
+            with open(datafile, 'r') as f:
+                data = json.load(f)
+            
+            data = itertools.dropwhile(lambda x: x['Country Name'] != 'Afghanistan', data)
+
+            gdps = []
+            for d in data:
+                gdps.append(GDP(
+                    country=d['Country Name'],
+                    country_code=d['Country Code'],
+                    year=d['Year'],
+                    gdp=d['Value']
+                ))
+
+            GDP.objects.bulk_create(gdps)
+            print('--done--')
+        
