5d6eac62e761d954c0aedd7eb51f8985f57972b7
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-02 12:40:48 +0200

    Update views.py

--- gdp/views.py
+++ gdp/views.py
@@ -4,5 +4,12 @@ from gdp.models import GDP
 
 # Create your views here.
 def index(request):
+    # define which year we want the data from
     max_year = GDP.objects.aggregate(max_yr=Max('year'))
+    year = request.GET.get('year', max_year)
+
+    # define number of countries to fetch
+    count = int(request.GET.get('count', 10))
+
+    gpps = GDP.objects.filter(year=year).order_by('gdp').reverse()[:count]
     return render(request, 'index.html')
